27a2e994d4f2bbf9fa9ba51f2d85d6064c389503
Antariksh C <antarikshc@gmail.com>
2019-10-03 00:14:53 +0530

    fix: misc migration errors
    Signed-off-by: Antariksh C <antarikshc@gmail.com>

--- app/src/androidTest/java/com/antarikshc/theguardiannews/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/antarikshc/theguardiannews/ExampleInstrumentedTest.java
@@ -1,11 +1,6 @@
 package com.antarikshc.theguardiannews;
 
-import static org.junit.Assert.assertEquals;
-
-import android.content.Context;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
-import org.junit.Test;
+import androidx.test.runner.AndroidJUnit4;
 import org.junit.runner.RunWith;
 
 /**
@@ -15,11 +10,5 @@ import org.junit.runner.RunWith;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.antarikshc.theguardiannews", appContext.getPackageName());
-    }
 }
